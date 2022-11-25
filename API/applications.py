dc64f9f9b0c8c4ba67cc0f24d907ff246894131f
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2022-11-25 09:15:05 -0300

    Delete method implemented

--- API/applications.py
+++ API/applications.py
@@ -1,7 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
+from flask import Flask,request
 from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 db = SQLAlchemy(app)
@@ -37,3 +36,20 @@ def get_drink(id):
         "name": drink.name,
         "description": drink.description
     }
+
+@app.route('/drinks',methods=['POST'])
+def add_drink():
+    drink = Drink(name=request.json['name'],description=request.json['description'])
+    db.session.add(drink)
+    db.session.commit()
+    return {'id': drink.id}
+
+@app.route('/drinks/<id>',methods=['DELETE'])
+def delete_drink(id):
+    drink = Drink.query.get(id)
+    if drink is None:
+        return {"error":"item not found."}
+    db.session.delete(drink)
+    db.session.commit()
+    return {"message": "Item deleted."}
+
